0940e63d6725961182939008fface0121e30abef
FZoltan40 <farkasz@kkszki.hu>
2024-11-25 08:39:26 +0100

    Post endpoint is ready to use.

---
+++ ComputerApiHetfo/Controllers/OsystemController.cs
@@ -0,0 +1,35 @@
+﻿using ComputerApiHetfo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerApiHetfo.Controllers
+{
+    [Route("osystem")]
+    [ApiController]
+    public class OsystemController : ControllerBase
+    {
+        private readonly ComputerContext computerContext;
+        public OsystemController(ComputerContext computerContext)
+        {
+            this.computerContext = computerContext;
+        }
+
+        [HttpPost]
+        public ActionResult<Osystem> Post(CreateOsDto createOsDto)
+        {
+            var os = new Osystem
+            {
+                Id = Guid.NewGuid(),
+                Name = createOsDto.Name
+            };
+
+            if (os != null)
+            {
+                computerContext.Osystems.Add(os);
+                computerContext.SaveChanges();
+                return StatusCode(201, os);
+            }
+
+            return BadRequest();
+        }
+    }
+}

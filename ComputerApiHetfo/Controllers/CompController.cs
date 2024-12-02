c2bbb3501f2fd1e4df50551aac63af4068872eef
FZoltan40 <farkasz@kkszki.hu>
2024-12-02 07:46:44 +0100

    Computer delete endpoint is ready..

---
+++ ComputerApiHetfo/Controllers/CompController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerApiHetfo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompController : ControllerBase
+    {
+    }
+}
--- ComputerApiHetfo/obj/Debug/net8.0/ComputerApiHetfo.AssemblyInfo.cs
+++ ComputerApiHetfo/obj/Debug/net8.0/ComputerApiHetfo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComputerApiHetfo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ec6ce885ffbd5132b433d59829a990fc5ba81f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c3246ae71aeae49e6d7ec2128940cd36043d3c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComputerApiHetfo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComputerApiHetfo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
